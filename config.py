285ea9c6bffc498aabe9c74ed388306a695e26cf
MalahitTV <mosbka.37@yandex.ru>
2020-08-22 15:40:25 +0300

    fixed param values

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ BN_MOMENTUM = 0.99
 CLASSES = 100
 DROPOUT = 0.8
 MIN_DEPTH = 3
-ALPHA = .5
+ALPHA = .75
 
 # data parameters
 IM_SIZE = 128
 NORMALIZE = ([0.24, 0.25, 0.26],
              [.31, .32, .33])
-BATCH_SIZE = 32
+BATCH_SIZE = 512
 
 # optimizer parameters
 LR = 1e-1
@@ -19,7 +19,7 @@ OPTIM_MOMENTUM = 0.9
 WEIGHT_DECAY = 1e-5
 
 # train parameters
-EPOCHS = 3
+EPOCHS = 2
 DEVICE = 'cuda'
 
 # test parameters
