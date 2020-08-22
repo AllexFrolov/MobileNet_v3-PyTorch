af9bd3af74ee1bf13ddb1dc1cb245c7a2de0d833
MalahitTV <mosbka.37@yandex.ru>
2020-08-22 12:25:20 +0300

    Added data, optimizer and training hyper-parameters

--- config.py
+++ config.py
@@ -1,5 +1,20 @@
 
+# model parameters
 BN_MOMENTUM = 0.99
 CLASSES = 100
 DROPOUT = 0.8
+MIN_DEPTH = 3
+ALPHA = .5
 
+# data parameters
+IM_SIZE = 128
+BATCH_SIZE = 32
+
+# optimizer parameters
+LR = 1e-1
+OPTIM_MOMENTUM = 0.9
+WEIGHT_DECAY = 1e-5
+
+# train parameters
+EPOCHS = 3
+DEVICE = 'cuda'
