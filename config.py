a7b6c12f015f49ee6c6f3be31adaaec4fda7bb4b
MalahitTV <mosbka.37@yandex.ru>
2020-08-22 15:03:46 +0300

    Added transforms.Normalize parameters, Added MODEL_SIZE parameter. Added DATA_FOLDER parameter

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 
 # model parameters
+MODEL_SIZE = 'small'
 BN_MOMENTUM = 0.99
 CLASSES = 100
 DROPOUT = 0.8
@@ -8,6 +9,8 @@ ALPHA = .5
 
 # data parameters
 IM_SIZE = 128
+NORMALIZE = ([0.24, 0.25, 0.26],
+             [.31, .32, .33])
 BATCH_SIZE = 32
 
 # optimizer parameters
@@ -18,3 +21,6 @@ WEIGHT_DECAY = 1e-5
 # train parameters
 EPOCHS = 3
 DEVICE = 'cuda'
+
+# test parameters
+DATA_FOLDER = 'data/test_data/'
