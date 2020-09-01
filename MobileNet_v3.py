fa9acda0daee2a0ce8d304195e35c1aeecaa5011
MalahitTV <mosbka.37@yandex.ru>
2020-09-02 00:35:19 +0300

    Added annotations and descriptions

--- MobileNet_v3.py
+++ MobileNet_v3.py
@@ -6,11 +6,11 @@ import torch.nn as nn
 
 
 class CorrectDepth:
-    def __init__(self, alpha, min_depth=3):
+    def __init__(self, alpha: float, min_depth: int = 3):
         self.alpha = alpha
         self.min_depth = min_depth
 
-    def __call__(self, depth):
+    def __call__(self, depth: int) -> int:
         """
             Adjust a depth value
             :param depth: (int) Depth/channels
@@ -19,12 +19,19 @@ class CorrectDepth:
         return max(self.min_depth, int(depth * self.alpha))
 
 
-def get_model_params(size, classes, c_d):
+def get_model_params(architecture: str, classifier_output: int, c_d: CorrectDepth) -> tuple:
+    """
+    Return corrected architecture parameters
+    :param architecture: (str) Architecture should be "large" or "small"
+    :param classifier_output: output from classifier layer
+    :param c_d: (CorrectDepth) Depth corrector
+    :return: (tuple of namedtuple) corrected architecture parameters
+    """
     BNeck = namedtuple('bneck', ('in_c', 'exp_c', 'out_c', 'k', 'se', 'nl', 's'))
     Conv = namedtuple('conv', ('in_c', 'out_c', 'k', 'bn', 'se', 'nl', 's'))
     Pool = namedtuple('pool', ('in_c', 'exp_c', 'out_c', 'k', 'se', 'nl', 's'))
 
-    if size == 'large':
+    if architecture == 'large':
         return (
             Conv(3, c_d(16), 3, True, False, 'HS', 2),  # 224
             BNeck(c_d(16), c_d(16), c_d(16), 3, False, 'RE', 1),  # 112
@@ -43,11 +50,11 @@ def get_model_params(size, classes, c_d):
             BNeck(c_d(160), c_d(960), c_d(160), 5, True, 'HS', 1),  # 7
             BNeck(c_d(160), c_d(960), c_d(160), 5, True, 'HS', 1),  # 7
             Conv(c_d(160), c_d(960), 1, True, False, 'HS', 1),  # 7
-            Pool(c_d(960), '-', '-', '-', False, '-', 1),  # 7
+            Pool(c_d(960), '-', '-', '-', False, None, 1),  # 7
             Conv(c_d(960), c_d(1280), 1, False, False, 'HS', 1),  # 1
-            Conv(c_d(1280), classes, 1, False, False, '-', 1),  # 1
+            Conv(c_d(1280), classifier_output, 1, False, False, None, 1),  # 1
         )
-    elif size == 'small':
+    elif architecture == 'small':
         return (
             Conv(3, c_d(16), 3, True, False, 'HS', 2),  # 224
             BNeck(c_d(16), c_d(16), c_d(16), 3, True, 'RE', 2),  # 112
@@ -62,9 +69,9 @@ def get_model_params(size, classes, c_d):
             BNeck(c_d(96), c_d(96), c_d(96), 5, True, 'HS', 1),  # 7
             BNeck(c_d(96), c_d(576), c_d(96), 5, True, 'HS', 1),  # 7
             Conv(c_d(96), c_d(576), 1, True, True, 'HS', 1),  # 7
-            Pool(c_d(576), '-', '-', '-', False, '-', 1),  # 7
+            Pool(c_d(576), '-', '-', '-', False, None, 1),  # 7
             Conv(c_d(576), c_d(1024), 1, True, False, 'HS', 1),  # 1
-            Conv(c_d(1024), classes, 1, True, False, '-', 1),  # 1
+            Conv(c_d(1024), classifier_output, 1, True, False, None, 1),  # 1
         )
     else:
         raise ValueError('size must be "large" or "small"')
@@ -77,8 +84,10 @@ class BaseLayer(nn.Module):
             return nn.ReLU()
         elif nl == 'HS':
             return nn.Hardswish()
-        else:
+        elif nl is None:
             return None
+        else:
+            raise ValueError('nl should be "RE", "HS" or None')
 
     @staticmethod
     def same_padding(kernel_size):
@@ -86,93 +95,110 @@ class BaseLayer(nn.Module):
 
 
 class SqueezeAndExcite(nn.Module):
-    def __init__(self, in_c, out_c):
+    def __init__(self, channels: int):
         super().__init__()
-        exp_c = math.ceil(in_c / 4)
-        self.model = nn.Sequential(
-                nn.AdaptiveAvgPool2d(1),
-                nn.Conv2d(in_c, exp_c, 1),
-                nn.ReLU(),
-                nn.Conv2d(exp_c, out_c, 1),
-                nn.Hardsigmoid()
-            )
 
-    def forward(self, inputs):
-        return torch.mul(inputs, self.model(inputs))
+        sqz_channels = math.ceil(channels / 4)
+        self.sequential = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(channels, sqz_channels, 1),
+            nn.ReLU(),
+            nn.Conv2d(sqz_channels, channels, 1),
+            nn.Hardsigmoid()
+        )
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        return torch.mul(inputs, self.sequential(inputs))
 
 
 class DepthWiseConv(BaseLayer):
-    def __init__(self, channels, stride, k_size, nl):
+    def __init__(self, channels: int, kernel_size: int or tuple,
+                 non_linear: str, stride: int = 1):
         super().__init__()
 
         self.depth_wise = nn.Conv2d(channels,
                                     channels,
-                                    k_size,
+                                    kernel_size,
                                     stride,
-                                    self.same_padding(k_size),
+                                    self.same_padding(kernel_size),
                                     groups=channels)
 
-        self.non_linear = self.choice_nl(nl)
+        self.non_linear = self.choice_nl(non_linear)
         self.normalization = nn.BatchNorm2d(channels)
 
-    def forward(self, inputs):
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         out = self.depth_wise(inputs)
         return self.non_linear(self.normalization(out))
 
 
 class DepthWiseSepConv(nn.Module):
-    def __init__(self, in_c, out_c, s, k, nl, se):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int or tuple,
+                 squeeze_excite_add: bool, non_linear: str, stride: int = 1):
         super().__init__()
-        self.se = se
-        # add squeeze and excite
-        if self.se:
-            self.sae = SqueezeAndExcite(in_c, in_c)
-        self.depth_wise_conv = DepthWiseConv(in_c, s, k, nl)
-        self.point_wise = nn.Conv2d(in_c, out_c, 1)
+        # add Squeeze and Excitation block
+        if squeeze_excite_add:
+            self.sae = SqueezeAndExcite(in_channels)
+        else:
+            self.sae = None
 
-    def forward(self, inputs):
+        self.depth_wise_conv = DepthWiseConv(in_channels, kernel_size, non_linear, stride)
+        self.point_wise = nn.Conv2d(in_channels, out_channels, 1)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         out = self.depth_wise_conv(inputs)
-        if self.se:
+        if self.sae is not None:
             out = self.sae(out)
         out = self.point_wise(out)
-
         return out
 
 
 class BottleNeck(BaseLayer):
-    def __init__(self, in_c, exp_c, out_c, k, se, nl, s):
+    def __init__(self, in_channels: int, exp_channels: int, out_channels: int,
+                 kernel_size: int or tuple, squeeze_excite_add: bool,
+                 non_linear: str, stride: int = 1):
         super().__init__()
-        self.non_linear = self.choice_nl(nl)
-        self.conv = nn.Conv2d(in_c, exp_c, 1)
-        self.depth_wise_sep = DepthWiseSepConv(exp_c, out_c, s, k, nl, se)
-        self.normalization_bn = nn.BatchNorm2d(exp_c)
-        self.normalization_out = nn.BatchNorm2d(out_c)
+        self.non_linear = self.choice_nl(non_linear)
+        self.expansion_layer = nn.Conv2d(in_channels, exp_channels, 1)
+        self.depth_wise_sep = \
+            DepthWiseSepConv(exp_channels, out_channels, kernel_size,
+                             squeeze_excite_add, non_linear, stride)
 
-    def forward(self, inputs):
-        out = self.non_linear(self.normalization_bn(self.conv(inputs)))
+        self.normalization_bn = nn.BatchNorm2d(exp_channels)
+        self.normalization_out = nn.BatchNorm2d(out_channels)
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        out = self.expansion_layer(inputs)
+        out = self.normalization_bn(out)
+        out = self.non_linear(out)
         out = self.depth_wise_sep(out)
         out = out + inputs if inputs.size() == out.size() else out
         return self.normalization_out(out)
 
 
 class Convolution(BaseLayer):
-    def __init__(self, in_c, out_c, k, bn, se, nl, s):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int or tuple,
+                 batch_norm_add: bool, squeeze_excite_add: bool, non_linear: str, stride: int = 1):
         super().__init__()
-        self.se = se
-        self.bn = bn
-        if self.se:
-            self.sae = SqueezeAndExcite(out_c, out_c)
-        if self.bn:
-            self.normalization = nn.BatchNorm2d(out_c)
+        # add Squeeze and Excitation block
+        if squeeze_excite_add:
+            self.sae = SqueezeAndExcite(out_channels)
+        else:
+            self.sae = None
+        # add batch normalization
+        if batch_norm_add:
+            self.normalization = nn.BatchNorm2d(out_channels)
+        else:
+            self.normalization = None
 
-        self.conv = nn.Conv2d(in_c, out_c, k, s, self.same_padding(k))
-        self.non_linear = self.choice_nl(nl)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size,
+                              stride, self.same_padding(kernel_size))
+        self.non_linear = self.choice_nl(non_linear)
 
-    def forward(self, inputs):
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         out = self.conv(inputs)
-        if self.se:
+        if self.sae is not None:
             out = self.sae(out)
-        if self.bn:
+        if self.normalization is not None:
             out = self.normalization(out)
         if self.non_linear is not None:
             out = self.non_linear(out)
@@ -180,7 +206,7 @@ class Convolution(BaseLayer):
 
 
 def get_model(classes, size='small', alpha=1., dropout=0.8, min_depth=3):
-    correct_depth = CorrectDepth(alpha,  min_depth)
+    correct_depth = CorrectDepth(alpha, min_depth)
     parameters = get_model_params(size, classes, correct_depth)
     model = nn.Sequential()
     for ind, param in enumerate(parameters[:-1]):
